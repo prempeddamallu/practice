99d9d92e99260f0640536c701637a94f801f90b6
prempeddamallu <prem.peddamallu@datagrokr.com>
2024-08-21 13:32:21 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class hello{
     public static void main(Stribg args[]){
        System.out.println("Hello world"); 
        System.out.println("Good Afternoon"); 
+       System.out.println("This is sample code"); 
     }
 }
